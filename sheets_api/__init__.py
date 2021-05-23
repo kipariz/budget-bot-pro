c344c169eda8abb85f039ba81c817e3bb28680d5
kipariz <deanis187@gmail.com>
2021-05-23 14:25:38 +0300

    log for creds

--- sheets_api/__init__.py
+++ sheets_api/__init__.py
@@ -2,6 +2,7 @@ import os.path
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from collections import namedtuple
+import json
 
 here = os.path.dirname(__file__)
 root = os.path.abspath(os.path.join(here, os.pardir))
@@ -12,6 +13,10 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 creds = None
 
 try:
+    with open(config_path) as config_file:
+        creds_json = json.load(config_file)
+        print(creds_json)
+
     creds = service_account.Credentials.from_service_account_file(config_path, scopes=SCOPES)
 except:
     print("Problem with creds!")
